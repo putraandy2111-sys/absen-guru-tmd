ad4e419868dcc7742245049247daceb34eb32bb2
Andy <putraandy221@gmail.com>
2026-08-08 09:38:14 +0700

    feat: tambah PWA support (manifest, service worker, icon)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 const appState = {
   user: null,
   today: null,
